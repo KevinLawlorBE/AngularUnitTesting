4ee81dfacec8733860a64446b1933bfd197fdb2a
Kevin Lawlor <kevin.lawlor@bearingpoint.com>
2022-09-08 12:12:43 +0100

    Removing tests for 'before'

--- src/app/quotes/service/quote.service.spec.ts
+++ src/app/quotes/service/quote.service.spec.ts
@@ -8,15 +8,8 @@ describe("QuoteService", () => {
     service = new QuoteService();
   });
 
-  it("should create a post in an array", () => {
-    const qouteText = "This is my first post";
-    service.addNewQuote(qouteText);
-    expect(service.quoteList.length).toBeGreaterThanOrEqual(1);
+  it("should create service", () => {
+    expect(service).toBeTruthy();
   });
 
-  it("should remove a created post from the array of posts", () => {
-    service.addNewQuote("This is my first post");
-    service.removeQuote(0);
-    expect(service.quoteList.length).toBeLessThan(1);
-  });
 });
